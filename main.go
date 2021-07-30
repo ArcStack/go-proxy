3744de13065c95cd17e9a6249f79756854ca65ff
stephen <stephen.ma@anu.edu.au>
2021-07-30 14:36:28 +1000

    BUILD: a quick plugin of secure with development mode.

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"github.com/unrolled/secure"
+	"net/http"
 )
 
+var myHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("hello world"))
+	if err != nil {
+		return
+	}
+})
+
 func main() {
-	fmt.Println("Hello, proxy")
+	secureMiddleware := secure.New(secure.Options{IsDevelopment: true})
+	app := secureMiddleware.Handler(myHandler)
+	err := http.ListenAndServe("127.0.0.1:3333", app)
+	if err != nil {
+		return
+	}
 }
